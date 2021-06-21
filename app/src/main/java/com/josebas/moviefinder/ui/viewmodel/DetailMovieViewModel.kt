1fa7aafc8bdd870ec96876fc3df49d14a7336f8d
josebas <joseb.twelve@gmail.com>
2021-06-21 15:50:12 -0500

    added DetailMovieViewModel

---
+++ app/src/main/java/com/josebas/moviefinder/ui/viewmodel/DetailMovieViewModel.kt
@@ -0,0 +1,13 @@
+package com.josebas.moviefinder.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.josebas.moviefinder.domain.Movie
+
+class DetailMovieViewModel: ViewModel() {
+    private val movieLiveData = MutableLiveData<Movie>()
+
+    fun setMovie(movie: Movie) {
+        movieLiveData.value = movie
+    }
+}
